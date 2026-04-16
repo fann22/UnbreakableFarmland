507d3efbc76da840ea8580d1ad92fcd88e307c83
Fann22 <rilisan5@gmail.com>
2026-04-16 20:35:56 +0700

    Bruh.

--- src/mod/BDSE.cpp
+++ src/mod/BDSE.cpp
@@ -353,7 +353,7 @@ bool BDSE::enable() {
                 PlaySoundPacket packet(PlaySoundPacketPayload(
                     "random.orb",
                     player->getPosition(),
-                    1f, // volume
+                    1.0f, // volume
                     1.2f  // pitch
                 ));
                 player->sendNetworkPacket(packet);
